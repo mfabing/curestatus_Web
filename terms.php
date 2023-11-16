1abc6069549b0bfff77acf70b24cfd018281e8e2
Michael Fabing <michaelfabing@hotmail.com>
2023-11-16 13:36:37 -0500

    add the cookies, privacy, terms

---
+++ terms.php
@@ -0,0 +1,43 @@
+<?php
+include_once(__DIR__.'/inc/secure.php');
+$title = $description = 'Curestatus Inc - Terms and Conditions';
+?>
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="auto">
+<head>
+	<?php include_once(__DIR__.'/inc/head.php'); ?>
+</head>
+<body>
+	<?php include_once(__DIR__.'/inc/nav.php'); ?>
+	<main class="container my-3">
+		<div class="row">
+			<div class="col-12">
+				<h1>Curestatus Inc - Terms and Conditions</h1>
+				<p><strong>Last Updated</strong>: November 13, 2023</p>
+				<p>Welcome to Curestatus.ai, the platform owned and operated by Curestatus Inc. These Terms and Conditions ("Terms") outline the rules and regulations for the use of <a href="/">Curestatus.ai</a>. Please read these Terms carefully before accessing or using our services. By using Curestatus.ai, you accept and agree to be bound by these Terms. If you do not agree to these Terms, please refrain from using our Website.</p>
+				<p><strong>1. Acceptance of Terms</strong></p>
+				<p>1.1 Applicability: These Terms apply to all users of Curestatus.ai, including but not limited to visitors, contributors of content, and users who provide information.</p>
+				<p>1.2 Modifications: Curestatus Inc reserves the right to modify or replace these Terms at any time. By continuing to use the Website after any such changes, you acknowledge acceptance of the modified Terms.</p>
+				<p><strong>2. Use of the Website</strong></p>
+				<p>2.1 Accuracy of Information: Curestatus Inc acknowledges that AI-generated content on the Website may contain inaccuracies. We do not warrant the accuracy, completeness, or usefulness of any information and disclaim responsibility for any inaccuracies.</p>
+				<p>2.2 Health Information Disclaimer: Curestatus.ai may provide information on health conditions and medical research. However, this information is not medical advice and should not be considered a substitute for consulting a qualified medical professional. Always seek the advice of your physician or other qualified health provider.</p>
+				<p>2.3 No Collection of PHI: Curestatus Inc explicitly states that no Personally Identifiable Health Information (PHI) is collected or managed on the Website. All user information is handled in accordance with our Privacy Policy.</p>
+				<p><strong>3. User Conduct</strong></p>
+				<p>3.1 Social Features: The Website may include social features. Users are expected to adhere to community guidelines and refrain from engaging in any conduct that is harmful, offensive, or violates applicable laws.</p>
+				<p>3.2 Prohibited Activities: Users agree not to engage in any activity that could harm, disable, overburden, or impair the Website. Unauthorized access attempts are strictly prohibited.</p>
+				<p><strong>4. Limitation of Liability</strong></p>
+				<p>4.1 Disclaimer: Curestatus Inc, its affiliates, and its respective officers, directors, employees, agents, and representatives disclaim any liability for direct, indirect, incidental, consequential, or punitive damages arising from the use or inability to use the Website.</p>
+				<p>4.2 No Warranty: Curestatus Inc provides the Website "as is" without any warranties, express or implied. We do not warrant that the Website will be error-free or uninterrupted.</p>
+				<p><strong>5. Governing Law</strong></p>
+				<p>5.1 Jurisdiction: These Terms are governed by and construed in accordance with the laws of the United States and the European Union. Any disputes shall be resolved through arbitration in accordance with the rules of the American Arbitration Association.</p>
+				<p><strong>6. Changes to Terms</strong></p>
+				<p>6.1 Notification: Users will be notified of significant changes to these Terms. Continued use of the Website after such changes constitutes acceptance of the modified Terms.</p>
+				<p><strong>7. Contact Information</strong></p>
+				<p>If you have any questions about these Terms and Conditions, please contact us at <a href="javascript:window.location.href=atob('<?php echo base64_encode('mailto:info@curestatus.ai?subject=Privay Policy') ?>')">via email</a>.</p>
+				<p>Thank you for using Curestatus.ai!</p>
+			</div>
+		</div>
+	</main>
+	<?php include_once(__DIR__.'/inc/endScripts.php'); ?>
+</body>
+</html>
