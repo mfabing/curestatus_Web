c9810472ab7a563dbefcafd762268ab5711a415e
Michael Fabing <michaelfabing@hotmail.com>
2023-11-16 08:56:44 -0500

    text change in the pre-register page

--- preRegister/index.php
+++ preRegister/index.php
@@ -2,7 +2,7 @@
 include_once(__DIR__.'/../inc/secure.php');
 $title = 'CureStatus | Engage with the path to a cure for 7,940 conditions and diseases.';
 $description = 'Tracking the path of a cure for 9,734 conditions';
-$description = 'Pre-register to our curestatus.ai app and stay inform when we launch';
+$description = 'Sign up to pre-register for curestatus membership and get launch updates.';
 ?>
 <!DOCTYPE html>
 <html lang="en" data-bs-theme="auto">
@@ -19,14 +19,14 @@ $description = 'Pre-register to our curestatus.ai app and stay inform when we la
 				<div id="result1"></div>
 				<form hx-post="register.php" hx-target="#result1" hx-indicator="#spinner1" method="POST" role="form" hx-replace-target="true" id="registerForm">
 					<div class="form-group mb-3">
-						<label class="form-control-label" for="name">Your Name:</label>
+						<label class="form-control-label" for="name">Name:</label>
 						<input type="text" name="name" id="name" class="form-control" required="required" minlength="3">
 					</div>
 					<div class="form-group mb-3">
-						<label class="form-control-label" for="email">Your Email address:</label>
+						<label class="form-control-label" for="email">Email address:</label>
 						<input type="email" name="email" id="email" class="form-control" required="required" minlength="5">
 					</div>
-					<button type="submit" class="btn btn-dark w250">Pre-Register<i class="bi bi-box-arrow-in-right"></i> <span id="spinner1" class="my-indicator">Sending... <i class="fas fa-spinner fa-spin"></i></span></button>
+					<button type="submit" class="btn btn-dark w250">Join Us<i class="bi bi-box-arrow-in-right"></i> <span id="spinner1" class="my-indicator">Sending... <i class="fas fa-spinner fa-spin"></i></span></button>
 				</form>
 			</div>
 		</div>
